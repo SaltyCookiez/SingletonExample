bfc797cdc9712e76bd706524094ba215d115737e
Moris Vider <opilane@HP-CZC3217062>
2024-10-10 12:47:28 +0300

    Add Singleton3 example

--- SingletonExample/Singleton3.cs
+++ SingletonExample/Singleton3.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace SingletonExample
 {
-    internal class Singleton3
+    public sealed class Singleton3
     {
+        Singleton3() { }
+        private static readonly object _lock = new object ();
+        private static Singleton3 instance = null;
+        public static Singleton3 Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (_lock)
+                        {
+                            if (instance == null)
+                            {
+                                instance = new Singleton3();
+                            }
+                        }
+                }
+                return instance;
+            }
+        }
     }
 }
