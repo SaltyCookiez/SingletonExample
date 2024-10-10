f2ab4a20588415ed5e47ca669d7c96d9a4de6a8e
Moris Vider <opilane@HP-CZC3217062>
2024-10-10 12:49:24 +0300

    Add Singleton5 example

--- SingletonExample/Singleton5.cs
+++ SingletonExample/Singleton5.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace SingletonExample
 {
-    internal class Singleton5
+    public sealed class Singleton5
     {
+        private Singleton5()
+        {
+        }
+        private static readonly Lazy<Singleton5> lazy = new Lazy<Singleton5>(() => new Singleton5());
+        public static Singleton5 Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
     }
 }
