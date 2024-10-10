133da9fc02dd6070e079fe8626f52937b9eb0a20
Moris Vider <opilane@HP-CZC3217062>
2024-10-10 12:47:55 +0300

    Add Singleton4 example

--- SingletonExample/Singleton4.cs
+++ SingletonExample/Singleton4.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace SingletonExample
 {
-    internal class Singleton4
+    public sealed class Singleton4
     {
+        private static readonly Singleton4 instance = new Singleton4();
+        static Singleton4()
+        {
+        }
+        private Singleton4()
+        {
+        }
+        public static Singleton4 Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
     }
 }
